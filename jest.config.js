925f2b95bd079006135421342f9deaa6b9659f36
Dương Trần Thái Sơn <sonduongtranthai@gmail.com>
2021-07-23 09:22:42 -0700

    Initial commit

---
+++ jest.config.js
@@ -0,0 +1,6 @@
+const {defaults} = require('jest-config')
+module.exports = {
+  setupFilesAfterEnv: ["<rootDir>/jest.setup.ts"],
+  testPathIgnorePatterns: ["<rootDir>/.next/", "<rootDir>/node_modules/"],
+  moduleFileExtensions: [...defaults.moduleFileExtensions, 'ts', 'tsx'],
+};
